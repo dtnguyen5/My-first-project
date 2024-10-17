638c5468cefb7894ec8f4cfc1a24816e655a62d2
Honza <duy.nguyen@infis.cz>
2024-10-17 23:05:48 +0200

    pridaji animace pomoci javascriptu

---
+++ 1.web/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    // Najdeme elementy, které chceme animovat
+    const sekce1 = document.querySelector('.sekce1');
+    const container = document.querySelector('.container');
+    const mobile = document.querySelector('.mobile');
+
+    // Přidáme třídu visible
+    sekce1.classList.add('visible');
+    container.classList.add('visible');
+    mobile.classList.add('visible');
+});
+
