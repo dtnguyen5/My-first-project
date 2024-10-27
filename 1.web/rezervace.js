d5881f384005dafa15ddd7ef8873a4c4490fb5f3
Honza <duy.nguyen@infis.cz>
2024-10-27 22:28:53 +0100

    zmeny responsility

--- 1.web/rezervace.js
+++ 1.web/rezervace.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', () => {
-    // Najdeme elementy, které chceme animovat
+    console.log('Script loaded');  // Přidá log pro kontrolu
     const sekce1 = document.querySelector('.sekce1');
     const sekce2 = document.querySelector('.sekce2');
-
-    // Přidáme třídu 'visible' pro plynulou animaci
     sekce1.classList.add('visible');
     sekce2.classList.add('visible');
 });
